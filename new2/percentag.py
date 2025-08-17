deb4809af5ca231a093923127622ff5cde0f6f70
mueeddeveloper <mueedmajid501@gmail.com>
2025-08-17 11:14:22 +0530

    marking

---
+++ new2/percentag.py
@@ -0,0 +1,11 @@
+print("enter marks obtained in 4 subjects")
+math = int(input("maths:"))
+english = int(input("english:"))
+science = int(input("science:"))
+hindi = int(input("hindi:"))
+sum = math + english + science + hindi
+print("sum of maths, english, science and hindi is:", sum)
+perc = (sum/400)*100
+print("the child has scored:",perc,"%")
+
+      
