cd619f128b883b8a4e693a66fa7ae11cb4bc4da8
mueeddeveloper <mueedmajid501@gmail.com>
2025-08-20 23:14:04 +0530

    birthdays

---
+++ new2/birthday.py
@@ -0,0 +1,13 @@
+# Store birthdays in separate variables
+birthday1 = "12-01-2000"
+birthday2 = "25-03-1999"
+birthday3 = "07-07-2001"
+birthday4 = "15-08-1998"
+birthday5 = "30-12-2002"
+
+# Print birthdays
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
