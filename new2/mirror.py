e9ab8256501063df2246c86c9ebd147ea367edec
mueeddeveloper <mueedmajid501@gmail.com>
2025-12-01 11:21:30 +0530

    jwj

---
+++ new2/mirror.py
@@ -0,0 +1,9 @@
+n = 5
+
+for i in range(1, n+1):
+    if i == 1:
+        print(" " * (n - i) + "*")
+    elif i == n:
+        print("*" * n)
+    else:
+        print(" " * (n - i) + "*" + " " * (i - 2) + "*")
