833c6f7eaa6492326de35886fd1e75416d556479
mueeddeveloper <mueedmajid501@gmail.com>
2025-12-02 10:41:32 +0530

    hi

--- new2/shape.py
+++ new2/shape.py
@@ -2,7 +2,7 @@ import turtle
 turtle.Screen().bgcolor("green")
 turtle.Screen().setup(300,400)
 polygon = turtle.Turtle()
-num_sides = 6
+num_sides = 10
 side_length = 70
 angle = 360.0 / num_sides
 for x in range(num_sides):
